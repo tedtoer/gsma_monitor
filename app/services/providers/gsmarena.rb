df1c2d0b026a95e6068ece2182d8c93c8f211bf8
Artem Belankov <tedtoer@gmail.com>
2016-11-23 06:00:18 +0500

    added get functional for gsmarena

--- app/services/providers/gsmarena.rb
+++ app/services/providers/gsmarena.rb
@@ -6,15 +6,32 @@ class Providers::Gsmarena < Providers::Base
     phones.map do |phone|
       {
         title: phone.content,
-        id: phone.css('a').first.attribute('href').value[0..-5],
-        image_url: phone.css('img').first.attribute('src').value
+        id: phone.css('a').first.attribute('href').value[0..-5]
       }
     end
   end
 
   def get(id: nil)
     get_url = base_url_for_query + "/#{id}.php"
-    Nokogiri::HTML(open(get_url))
+    doc = Nokogiri::HTML(open(get_url))
+    phone = doc.css('.center-stage').first
+    specs = phone.css('.specs-spotlight-features').first
+    display_spec = specs.css('li.help.accented.help-display').first
+    camera_spec = specs.css('li.help.accented.help-camera').first
+    expansion_spec = specs.css('li.help.accented.help-expansion').first
+    battery_spec = specs.css('li.help.accented.help-battery').first
+
+    {
+      image_url: phone.css('.specs-photo-main a img').first.attribute('src').value,
+      display_size: display_spec.children[3].content[0..-2],
+      display_resolution: display_spec.children[4].content.split(' ')[0],
+      camera_photo: camera_spec.children[3].content.strip,
+      camera_video: camera_spec.children[4].content.strip,
+      ram: expansion_spec.children[3].content.strip,
+      cpu: expansion_spec.children[4].content.strip,
+      battery_bulk: battery_spec.children[3].content.strip,
+      battery_type: battery_spec.children[4].content.strip
+    }
   end
 
   private
